f02539c94c3f47b7b8fd08e18e863d697a9c5522
juanmal1 <juanmal_100@hotmail.com>
2022-08-23 01:44:29 -0300

    primer commit

---
+++ nuevo.js
@@ -0,0 +1,22 @@
+for (let i = 1; i <= 10; i++) {
+
+    
+    let ingresarNombre = prompt("Ingresar nombre");
+
+    alert(" Turno  N° " + i +" Nombre: " +ingresarNombre);
+    console.log(" Turno  N° " + i +" Nombre: " +ingresarNombre);
+
+    if (i=== 6){
+        alert('esta persona esta demorada por llegar tarde')
+        console.log ('esta persona esta demorada por llegar tarde');
+    }
+
+    if (i === 10){
+        alert ('llegue al N° maximo de turnos por dia');
+        console.log ('llegue al N° maximo de turnos por dia');
+        break;
+    }
+}
+
+
+
